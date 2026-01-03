6e58511cdb97d812edc1a59572282c6ea8ec15b0
Aniket <akene1999@gmail.com>
2026-01-03 15:09:11 +0530

    sample add backend for backup of tf.state file in s3 bucket

--- sample.tf
+++ sample.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region = "us-east-1"   # N. virginia
 }
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"   # Replace with your S3 bucket name
+    key    = "terraform.tfstate"  # Replace with your desired state file path
+    region = "us-east-1"   # N. virginia  
+  }
+}
 resource "aws_instance" "my_instance" {
   ami = "ami-068c0051b15cdb816"   # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t3.micro"
